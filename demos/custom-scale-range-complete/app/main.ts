d8a33f3911e2de0bb4f4678fc78d6651dc28deca
Matt Driscoll <MDriscoll@esri.com>
2020-03-04 13:34:37 -0800

    demo work

---
+++ demos/custom-scale-range-complete/app/main.ts
@@ -0,0 +1,39 @@
+import MapView = require("esri/views/MapView");
+import FeatureLayer = require("esri/layers/FeatureLayer");
+import Map = require("esri/Map");
+import CustomScaleRange = require("./CustomScaleRange");
+
+//----------------
+//  map setup
+//----------------
+
+const layer = new FeatureLayer({
+  portalItem: {
+    id: "f9e348953b3848ec8b69964d5bceae02"
+  },
+  outFields: ["SEASON"]
+});
+
+const map = new Map({
+  basemap: "gray-vector",
+  layers: [layer]
+});
+
+const view = new MapView({
+  container: "viewDiv",
+  map: map,
+  zoom: 4,
+  center: [-99, 39]
+});
+
+//----------------
+//  widget setup
+//----------------
+
+const widget = new CustomScaleRange({
+  view: view,
+  layer,
+  region: "us"
+});
+
+view.ui.add(widget, "top-right");
